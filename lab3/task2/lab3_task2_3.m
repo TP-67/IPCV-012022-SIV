ba338b2e628629d2bd3aec002adfba96d91ae435
TP-67 <bofg.fu@outlook.com>
2022-02-12 02:43:16 +0800

    submit

--- lab3/task2/lab3_task2_3.m
+++ lab3/task2/lab3_task2_3.m
@@ -1,16 +1,29 @@
+% Task: Image resizing
+
+% Load image
 img = imread('peppers.jpg');
 
+% Image resize
 img_resize1 = imresize(img, 0.5);
 img_resize2 = imresize(img, 0.25);
 img_resize3 = imresize(img, 0.1);
 
-
+% Create figure for different sets of images.
 figure(1);
 imshow(img);
+title('Original image');
+
+% Create figure for different sets of images.
 figure(2);
 imshow(img_resize1);
+title('Resizing rate 0.5');
+
+% Create figure for different sets of images.
 figure(3);
 imshow(img_resize2);
+title('Resizing rate 0.25');
+
+% Create figure for different sets of images.
 figure(4);
 imshow(img_resize3);
-% title('Original image');
+title('Resizing rate 0.1');
